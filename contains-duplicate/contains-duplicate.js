4b5ab1560ba38a892f6101f98062f09cc1231ebd
angginoor <139970333+angginoor@users.noreply.github.com>
2024-03-20 10:41:50 +0700

    Time: 82 ms, Memory: 60.5 MB - LeetHub

---
+++ contains-duplicate/contains-duplicate.js
@@ -0,0 +1,17 @@
+/**
+ * @param {number[]} nums
+ * @return {boolean}
+ */
+var containsDuplicate = function(nums) {
+    const setNums = new Set()
+    
+    for(let i = 0; i < nums.length; i++) {
+        if(setNums.has(nums[i])) {
+            return true
+        } else {
+            setNums.add(nums[i])
+        }
+    }
+    
+    return false
+};
